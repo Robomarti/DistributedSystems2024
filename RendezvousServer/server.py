52b8348f384983c6bef70fbfc143d7034885198b
Sami Männistö <sami.mannisto1@gmail.com>
2024-12-06 10:24:05 +0200

    Add back missing NEW_CLIENT! logic

--- RendezvousServer/server.py
+++ RendezvousServer/server.py
@@ -23,6 +23,7 @@ class Server(DatagramProtocol):
             self.clients[addr] = None
             print(f"Client connected: {addr}")
             self.player_order()
+            self.send_all(f"NEW_CLIENT!{addr[0]}:{addr[1]}", exclude=addr)
 
     def client_disconnection(self, addr):
         """Handle a client disconnection"""
@@ -37,6 +38,12 @@ class Server(DatagramProtocol):
         for index, client_addr in enumerate(self.clients.keys(), start=1):
             message = f"PLAYER_ORDER!{index}!{addresses}"
             self.transport.write(message.encode("utf-8"), client_addr)
+
+    def send_all(self, message, exclude=None):
+        """Send a message to all clients"""
+        for peer_address in self.clients:
+            if peer_address != exclude:
+                self.transport.write(message.encode("utf-8"), peer_address)
 
 if __name__ == '__main__':
     os.system("clear")
